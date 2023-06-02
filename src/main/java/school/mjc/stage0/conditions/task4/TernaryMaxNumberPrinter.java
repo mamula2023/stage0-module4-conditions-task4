e74a6e975a0c2d697a6bb8d665681ede6daed24d
mamula2023 <mamulashvili.2017@gmail.com>
2023-06-03 01:21:32 +0400

    stupid ternary

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first >= second) {
+            if (first >= third) System.out.println(first);
+            else System.out.println(third);
+        } else {
+            if (third > second) System.out.println(third);
+            else System.out.println(second);
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryOperator.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryOperator.java
@@ -3,7 +3,8 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryOperator {
     public static void main(String[] args) {
         int b = 16;
-        b = b __ 0 ? 10 : 23;
+        int temp = 0;
+        b = temp==0?10:23;
         System.out.println(b);
     }
 }
